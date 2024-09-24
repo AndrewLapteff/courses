5323f1a9dcc0623a2b2d86578815f16a833d8197
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-09-24 12:17:58 +0300

    Add autherization

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,19 +1,29 @@
+import { auth, signIn, signOut } from '@/auth'
 import { Navlink } from '@/components/shared/nav-link'
 import { Navbar } from '@/components/widgets/navbar'
+import SignIn from '@/components/widgets/signin'
+import SignOut from '@/components/widgets/signout'
 
 export const dynamic = 'force-dynamic'
 
-export default function Layout({
+export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const session = await auth()
+  console.log(session)
   return (
     <>
       <Navbar>
-        <Navlink href="/">Home</Navlink>
-        <Navlink href="/products">Products</Navlink>
-        <Navlink href="/orders">Orders</Navlink>
+        <div className="flex items-center">
+          <Navlink href="/">Home</Navlink>
+          <Navlink href="/products">Products</Navlink>
+          <Navlink href="/orders">Orders</Navlink>
+        </div>
+        <div className="flex items-center">
+          <div className="ml-4">{session ? <SignOut /> : <SignIn />}</div>
+        </div>
       </Navbar>
       <div className="container my-6">{children}</div>
     </>
--- components/widgets/navbar.tsx
+++ components/widgets/navbar.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react'
 
 export function Navbar({ children }: { children: ReactNode }) {
   return (
-    <nav className="bg-primary text-primary-foreground flex justify-center px-4">{children}</nav>
+    <nav className="bg-primary text-primary-foreground flex justify-between px-4">{children}</nav>
   )
 }
